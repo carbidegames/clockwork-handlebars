ca2ab81c6b3297d735be7f9de1cb8513514dad48
Layl <LaylConway@users.noreply.github.com>
2016-07-19 16:51:54 +0200

    Add layout support

--- src/lib.rs
+++ src/lib.rs
@@ -3,24 +3,24 @@ extern crate handlebars;
 extern crate rustc_serialize;
 extern crate webutil;
 
+use std::path::Path;
+use std::collections::BTreeMap;
 use clockwork::Module;
 use handlebars::Handlebars;
-use rustc_serialize::json::ToJson;
+use rustc_serialize::json::{Json, ToJson};
 use webutil::HtmlString;
 
 pub struct ViewRenderer {
     registry: Handlebars,
+    layout: String,
 }
 
 impl ViewRenderer {
-    pub fn new() -> Self {
+    pub fn new<D: AsRef<Path>, S: ToString>(directory: D, layout: S) -> Self {
         let mut registry = Handlebars::new();
 
-        //registry.register_template_string("number", source.to_string())
-        //    .ok().unwrap();
-
         // Scan the views directory for files
-        for entry in ::std::fs::read_dir("./views").unwrap() {
+        for entry in ::std::fs::read_dir(directory).unwrap() {
             let entry = entry.unwrap();
             let path = entry.path();
             let metadata = entry.metadata().unwrap();
@@ -32,21 +32,41 @@ impl ViewRenderer {
 
             // Add the template
             let template_name = path.file_stem().unwrap();
-            registry.register_template_file(template_name.to_str().unwrap(), &path).unwrap();
+            registry.register_template_file(template_name.to_str().unwrap(), &path)
+                .map_err(|e| format!("Failed to parse {:?}: {:?}", path, e))
+                .unwrap();
         }
 
         ViewRenderer {
             registry: registry,
+            layout: layout.to_string(),
         }
     }
 
     pub fn render<M: ToJson>(&self, view: &str, model: &M) -> HtmlString {
-        // TODO: Register views on new
+        // Render the specific view
+        let content = self.registry.render(view, model).unwrap();
 
-        let html = self.registry.render(view, model).unwrap();
+        // Render the vew into the layout
+        let template_model = TemplateModel {content: content};
+        let html = self.registry.render(&self.layout, &template_model).unwrap();
+
+        // Bless the result and pass it on
         HtmlString::bless(html)
     }
 }
 
 impl Module for ViewRenderer {
+}
+
+struct TemplateModel {
+    content: String
+}
+
+impl ToJson for TemplateModel {
+    fn to_json(&self) -> Json {
+        let mut m: BTreeMap<String, Json> = BTreeMap::new();
+        m.insert("content".into(), self.content.to_json());
+        m.to_json()
+    }
 }
